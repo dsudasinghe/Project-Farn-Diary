9d8f29648ef16cc03f210f278c84c1a76406d1fe
LalinduWenasara <49421049+LalinduWenasara@users.noreply.github.com>
2020-12-29 09:38:25 +0530

    added marker and info window

--- mapout.php
+++ mapout.php
@@ -9,11 +9,33 @@
 <script>
 function myMap() {
 var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
+  center:new google.maps.LatLng(7.0840,80.0098),
   zoom:5,
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+
+var mycenter = new google.maps.LatLng(7.0840,80.0098);
+
+
+var marker = new google.maps.Marker({position:mycenter,animation:google.maps.Animation.BOUNCE});
+
+
+marker.setMap(map);
+
+var infowindow = new google.maps.InfoWindow({
+  content:"test "
+});
+
+infowindow.open(map,marker);
+
+
 }
+
+
+
+
+
 </script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBheNEtrngM3cbowGS3tLPwoBXlswmmSb0&callback=myMap"></script>
