0b2b002bd708c6a982075dc17a291aa681e43718
LalinduWenasara <49421049+LalinduWenasara@users.noreply.github.com>
2021-01-12 00:38:31 +0530

    updates

--- messagerecive.php
+++ messagerecive.php
@@ -7,29 +7,42 @@
 	<div>
 		<table border="1">
 			<thead>
-				<th>Firstname</th>
-				<th>Lastname</th>
+				<th>sender</th>
+			
 				<th></th>
 			</thead>
 			<tbody>
 				<?php
-					include('conn.php');
-					$query=mysqli_query($conn,"select * from `user`");
+                    include('dbconnect.php');
+                    $logflag=2;
+					$query=mysqli_query($con,"SELECT * FROM `message` where receverid='$logflag' OR senderid='$logflag'" );
 					while($row=mysqli_fetch_array($query)){
 						?>
-						<tr>
-							<td><?php echo $row['firstname']; ?></td>
-							<td><?php echo $row['lastname']; ?></td>
+						<tr style="border: 2px solid #dedede;
+ padding: 10%;">
+							<td><?php echo $row['senderid']; ?></td>
+							<td><?php echo $row['message']; ?></td>
 							<td>
-								<a href="edit.php?id=<?php echo $row['userid']; ?>">Edit</a>
-								<a href="delete.php?id=<?php echo $row['userid']; ?>">Delete</a>
+								<a href="deletemessage.php?msgid=<?php echo $row['msgid'];?>">Delete</a>
 							</td>
 						</tr>
 						<?php
 					}
 				?>
 			</tbody>
-		</table>
-	</div>
+        </table>
+        
+    </div>
+    <div>
+   <form action="messagesend.php" method="post" >
+   <input style="display:none;" type="text" value="<?php echo $senderrr?>" name="receverid" />
+   <label for="lname">message:</label><br>
+   <input type="text" id="mess" name="message" value="enter your message here"><br><br>
+   <input class="btn btn-outline-success" type="submit" value="send">
+    </form>
+    </div>
+
+
+
 </body>
 </html>
