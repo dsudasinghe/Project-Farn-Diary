4780634e654030ac780cc281a525aff09c644390
dsudasinghe <dananjayasudasinghe@gmail.com>
2021-01-12 19:33:58 +0530

    updated

--- mapinput.php
+++ mapinput.php
@@ -4,13 +4,13 @@
 
 <h1>My First Google Map</h1>
 
-<div id="googleMap" style="width:100%;height:400px;"></div>
+<div id="googleMap" style="width:100%;height:800px;"></div>
 
 <script>
 function myMap() {
 var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
+  center:new google.maps.LatLng(6.9271,79.8612),
+  zoom:11,
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
