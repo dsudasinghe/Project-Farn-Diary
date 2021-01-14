3a19ab36abdce790b115847c283512dba7c2bd3c
LalinduWenasara <49421049+LalinduWenasara@users.noreply.github.com>
2021-01-14 15:09:19 +0530

    upload

--- mapinput2.php
+++ mapinput2.php
@@ -74,6 +74,135 @@ var dateTime = date+' '+time;
 
 
 
+
+
+
+
+
+
+<?php
+
+$senderid=1;
+
+
+
+   
+
+
+
+
+
+include 'dbconnect.php';
+if($con === false){
+	
+  die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+
+if (isset($_POST['submit'])) {
+  // do logic
+  $latitude= $_POST['latitude'];
+  $longitude= $_POST['longitude'];
+  $type= $_POST['type'];
+  $description= $_POST['description'];
+  $datetime= $_POST['datetime'];
+  $price= $_POST['price'];
+  $image = $_FILES['image']['name']; 
+  
+
+  $move=move_uploaded_file($_FILES["image"]["tmp_name"],"uploads/". $_FILES["image"]["name"]);	
+
+   
+    
+  $sql = "INSERT INTO product(latitude,longitude,price,datetime,type,description,image) values ('$latitude','$longitude','$price','$datetime','$type','$description','$senderid','$image')";
+  
+    
+  //excute query
+
+  
+  mysqli_query($con,$sql);
+
+
+
+ echo 
+"latitude:$latitude,longitude:$longitude,type:$type,description:$description,datetime:$datetime,price:$price";  
+
+  
+
+    
+if($move)
+  
+    
+{
+
+
+  echo " successfully uploaded";
+
+
+}else{
+
+
+  echo "Failed to upload";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  echo"all set";
+  
+
+
+  //Get text
+
+
+
+
+}
+ 
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php include 'navcommonfarmer.php'; ?>
 
 
--- mapinputpass.php
+++ mapinputpass.php
@@ -1,11 +1,32 @@
 <html>
+
+<?php session_start()
+?>
+
+
+
+
+
 <?php
 
+$senderid=1;
 
 
 
-include 'dbconnect.php'; 
-if (isset($_POST['latitude'])) {
+   
+
+
+
+
+
+include 'dbconnect.php';
+if($con === false){
+	
+  die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+
+if (isset($_POST['submit'])) {
   // do logic
   $latitude= $_POST['latitude'];
   $longitude= $_POST['longitude'];
@@ -13,20 +34,15 @@ if (isset($_POST['latitude'])) {
   $description= $_POST['description'];
   $datetime= $_POST['datetime'];
   $price= $_POST['price'];
- 
-
-
-
-
-/*
-
   $image = $_FILES['image']['name']; 
+  
+
+
   $move=move_uploaded_file($_FILES["image"]["tmp_name"],"uploads/". $_FILES["image"]["name"]);	
 
-*/
    
     
-  $sql = "INSERT INTO product(latitude,longitude,datetime,price) values ('$latitude','$longitude','$datetime','$price','$price')";
+  $sql = "INSERT INTO product(latitude,longitude,price,datetime,type,description,image) values ('$latitude','$longitude','$price','$datetime','$type','$description','$senderid','$image')";
   
     
   //excute query
@@ -46,18 +62,14 @@ if (isset($_POST['latitude'])) {
 
   echo"all set";
   
-}
+
 
   //Get text
-  else{
-
-
-    echo"shi*";
-
-  }
 
 
 
+
+}
  
 
 ?>
